f6aad38fdbc5b47b0355bcb65939ba3c01b3c9d1
GFSprog40 <gustavo.prog40@gmail.com>
2023-08-09 10:34:53 -0300

    comitando

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import routes from './routes'
+
+ dotenv.config()
+
+  const {PORT} = process.env
+
+  const app = express()
+
+  app.use(cors())
+  app.use(express.json())
+  app.use(routes)
+
+
+  app.listen(PORT, () => {  console.log(` 💀 Api Running on: ${PORT}`)})
+
+  export default app
