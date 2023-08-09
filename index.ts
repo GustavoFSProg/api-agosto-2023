27bc97bf3d94184ec285b1fdc44fcf21c36de304
GFSprog40 <gustavo.prog40@gmail.com>
2023-08-09 10:35:50 -0300

    routes

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import routes from './routes'
+import routes from './src/routes'
 
  dotenv.config()
 
